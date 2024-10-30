382a67948683d4ddea0c1cb02e0e00aa4f51360b
saedd <saaed.amini@gmail.com>
2024-10-30 10:01:37 +0330

    add the comment in single.php

--- single.php
+++ single.php
@@ -5,120 +5,61 @@ get_header();
 <body>
 
     <div class="container-fluid">
-        <div class="container">
-            <div class="row pt-5">
-                <div class="col-lg-8">
+        <div class=" container">
+            <div class="row pt-5 ">
+                <div class="col-lg-8 col-md-12">
                     <!-- News Detail Start -->
                     <div class="position-relative mb-3">
-                        <img class="img-fluid w-100" src="<?php echo get_the_post_thumbnail_url();?>" style="object-fit: cover;">
+                        <img class="img-fluid w-100" src="<?php echo get_the_post_thumbnail_url(); ?>" style="object-fit: cover;">
                         <div class="bg-white border border-top-0 p-4">
                             <div class="mb-3">
                                 <a class="badge badge-primary text-uppercase font-weight-semi-bold p-2 mr-2"
-                                    href="<?php echo get_category_link(get_the_category()[0]->term_id)?>"><?php echo get_the_category()[0]->name;?></a>
+                                    href="<?php echo get_category_link(get_the_category()[0]->term_id) ?>"><?php echo get_the_category()[0]->name; ?></a>
                                 <span class="text-body"><?php echo get_the_date('M D , Y'); ?></span>
                             </div>
-                            <h1 class="mb-3 text-secondary text-uppercase font-weight-bold"><?php echo get_the_title( )?></h1>
+                            <h1 class="mb-3 text-secondary text-uppercase font-weight-bold"><?php echo get_the_title() ?></h1>
 
-                             <!-- Post Content -->
-                              <?php the_content()?>
-                             <!-- Post Content -->
-
+                            <!-- Post Content -->
+                            <?php if (have_posts()) {
+                                while (have_posts()) {
+                                    the_post();
+                                    the_content();
+                                }
+                            } ?>
+                            <!-- Post Content -->
                         </div>
-                        <div class="d-flex justify-content-between bg-white border border-top-0 p-4">
+                        <div class="d-flex justify-content-between bg-white border border-top-0 p-4 flex-wrap">
                             <div class="d-flex align-items-center">
-                                <img class="rounded-circle mr-2" src="<?php echo get_avatar_url(get_the_author_meta('ID')); ?>" width="25" height="25" alt="">
-                                <span><?php the_author() ?></span>
+                                <img
+                                    class="rounded-circle mr-2"
+                                    src="<?php echo get_avatar_url(get_the_author_meta('ID')); ?>"
+                                    width="25"
+                                    height="25"
+                                    alt=" " />
+                                <small><?php the_author() ?></small>
                             </div>
                             <div class="d-flex align-items-center">
-                                <!-- <span class="ml-3"><i class="far fa-eye mr-2"></i>12345</span> -->
-                                <span class="ml-3"><i class="far fa-comment mr-2"></i><?php comments_number('No Comments', '1 Comment', '% Comment');?></span>
+                                <span class="ml-3"><i class="far fa-comment mr-2"></i><?php comments_number('No Comments', '1 Comment', '% Comment'); ?></span>
                             </div>
                         </div>
                     </div>
-                    <!-- News Detail End -->
-
-                    <!-- Comment List Start -->
-                    <div class="mb-3">
-                        <div class="section-title mb-0">
-                            <h4 class="m-0 text-uppercase font-weight-bold">3 Comments</h4>
-                        </div>
-                        <div class="bg-white border border-top-0 p-4">
-                            <div class="media mb-4">
-                                <img src="img/user.jpg" alt="Image" class="img-fluid mr-3 mt-1" style="width: 45px;">
-                                <div class="media-body">
-                                    <h6><a class="text-secondary font-weight-bold" href="">John Doe</a> <small><i>01 Jan 2045</i></small></h6>
-                                    <p>Diam amet duo labore stet elitr invidunt ea clita ipsum voluptua, tempor labore
-                                        accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.</p>
-                                    <button class="btn btn-sm btn-outline-secondary">Reply</button>
-                                </div>
-                            </div>
-                            <div class="media">
-                                <img src="img/user.jpg" alt="Image" class="img-fluid mr-3 mt-1" style="width: 45px;">
-                                <div class="media-body">
-                                    <h6><a class="text-secondary font-weight-bold" href="">John Doe</a> <small><i>01 Jan 2045</i></small></h6>
-                                    <p>Diam amet duo labore stet elitr invidunt ea clita ipsum voluptua, tempor labore
-                                        accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed eirmod ipsum.</p>
-                                    <button class="btn btn-sm btn-outline-secondary">Reply</button>
-                                    <div class="media mt-4">
-                                        <img src="img/user.jpg" alt="Image" class="img-fluid mr-3 mt-1"
-                                            style="width: 45px;">
-                                        <div class="media-body">
-                                            <h6><a class="text-secondary font-weight-bold" href="">John Doe</a> <small><i>01 Jan 2045</i></small></h6>
-                                            <p>Diam amet duo labore stet elitr invidunt ea clita ipsum voluptua, tempor
-                                                labore accusam ipsum et no at. Kasd diam tempor rebum magna dolores sed sed
-                                                eirmod ipsum.</p>
-                                            <button class="btn btn-sm btn-outline-secondary">Reply</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- Comment List End -->
-
-                    <!-- Comment Form Start -->
-                    <div class="mb-3">
-                        <div class="section-title mb-0">
-                            <h4 class="m-0 text-uppercase font-weight-bold">Leave a comment</h4>
-                        </div>
-                        <div class="bg-white border border-top-0 p-4">
-                            <form>
-                                <div class="form-row">
-                                    <div class="col-sm-6">
-                                        <div class="form-group">
-                                            <label for="name">Name *</label>
-                                            <input type="text" class="form-control" id="name">
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <div class="form-group">
-                                            <label for="email">Email *</label>
-                                            <input type="email" class="form-control" id="email">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="website">Website</label>
-                                    <input type="url" class="form-control" id="website">
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="message">Message *</label>
-                                    <textarea id="message" cols="30" rows="5" class="form-control"></textarea>
-                                </div>
-                                <div class="form-group mb-0">
-                                    <input type="submit" value="Leave a comment"
-                                        class="btn btn-primary font-weight-semi-bold py-2 px-3">
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                    <!-- Comment Form End -->
                 </div>
-                 <!-- News With Sidebar Start -->
-                 <?php get_template_part('/template-parts/sidbar','main')?>
-                 <!-- News With Sidebar End -->
+
+                <!-- Sidebar -->
+                <?php get_template_part('/template-parts/sidbar', 'main') ?>
+                <!-- Sidebar -->
+
             </div>
+        </div>
+        <div class=" container">
+            <div class="row pt-5">
+                <div class="col-12">
+                    <!-- Comments -->
+                    <?php comments_template('/comments.php'); ?>
+                    <!-- Comments -->
+                </div>
+            </div>
+
         </div>
     </div>
 
