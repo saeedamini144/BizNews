a49563e3c56a1a61e92c4ca9c1c78a39fa5363df
saedd <saaed.amini@gmail.com>
2024-11-03 14:57:56 +0330

    Change the pagination Name

--- archive.php
+++ archive.php
@@ -62,8 +62,8 @@ get_header();
                             <?php
                             the_posts_pagination(array(
                                 'mid_size'  => 2,
-                                'prev_text' => __('Previous', 'textdomain'),
-                                'next_text' => __('Next', 'textdomain'),
+                                'prev_text' => __('<', 'textdomain'),
+                                'next_text' => __('>', 'textdomain'),
                             ));
                             ?>
 
